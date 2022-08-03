9540b2e44453cbac1993f1c358357fd6706ccde8
TunarN <tunarzn@code.edu.az>
2022-08-03 17:43:02 +0400

    Tapsiriq

---
+++ Tapsiriq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tapsiriq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Add Sentences");
+            string sentence = Console.ReadLine();
+            int count = 0;
+            foreach (var item in sentence)
+            {
+
+                if (item == 'a')
+                {
+                    count++;
+
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("var");
+            }
+            else
+            {
+                Console.WriteLine("yoxdur");
+            }
+        }
+
+
+
+    }
+}
+
